d900917bbed77ce410d9f2e2f240e9cd50ee0dbf
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-03-20 20:41:49 -0300

    Update form.php

---
+++ form.php
@@ -0,0 +1,11 @@
+<form action="envio-email.php">
+
+<label for="nome">Nome</label>
+<input type="text" name="nome" id="nome">
+
+<label for="sobrenome">Sobrenome</label>
+<input type="text" name="sobrenome" id="sobrenome">
+
+<label for="email">E-maill</label>
+<input type="text" name="email" id="email">
+</form>
