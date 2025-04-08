eb8eb9aa7e75537299cd9c135261d554fb1476d7
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-04-08 20:53:34 -0300

    Update api-criar-usuario.php

--- api-criar-usuario.php
+++ api-criar-usuario.php
@@ -4,82 +4,111 @@ require "api.php";
 // LOGIN-CADASTRO.PHP
 // RESPONSÁVEL POR PEGAR O EMAIL E A SENHA DO FORMULÁRIO
 //  SE VALOR EMAIL E SENHA NÃO TIVER VALORES CAI NO ERRO / EMAIL E SENHAS SÃO OBRIGATORIOS
-$nome = $_POST['nome'] ?? null;
-$email = $_POST['email'] ?? null;
-$senha = $_POST['senha'] ?? null;
-$cpf = $_POST['cpf'] ?? null;
-$celular = $_POST['celular'] ?? null;
-$sexo = $_POST['sexo'] ?? null;
-$datanascimento = $_POST['datanascimento'] ?? null;
-$cep = $_POST['cep'] ?? null;
-$endereco = $_POST['endereco'] ?? null;
-$complemento = $_POST['complemento'] ?? null;
-$bairro = $_POST['bairro'] ?? null;
-$cidade = $_POST['cidade'] ?? null;
-$estado = $_POST['estado'] ?? null;
-$pjouclt = $_POST['pjouclt'] ?? null;
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+if ($method == 'POST') {
+    $nome = $_POST['nome'] ?? null;
+    $email = $_POST['email'] ?? null;
+    $senha = $_POST['senha'] ?? null;
+    $cpf = $_POST['cpf'] ?? null;
+    $celular = $_POST['celular'] ?? null;
+    $sexo = $_POST['sexo'] ?? null;
+    $datanascimento = $_POST['datanascimento'] ?? null;
+    $cep = $_POST['cep'] ?? null;
+    $endereco = $_POST['endereco'] ?? null;
+    $complemento = $_POST['complemento'] ?? null;
+    $bairro = $_POST['bairro'] ?? null;
+    $cidade = $_POST['cidade'] ?? null;
+    $estado = $_POST['estado'] ?? null;
+    $pjouclt = $_POST['pjouclt'] ?? null;
 
 
-// SE NÃO TIVER VALOR CAI NO NULL E É CONSIDERADO FALSO E CAI NO ERRO
-if (!$email || !$senha || !$pjouclt || !$nome || !$cpf || !$celular || !$sexo || !$datanascimento || !$cep || !$endereco || !$complemento || !$bairro || !$cidade || !$estado) {
-    echo json_encode(['sucess' => false, 'message' => 'Preencha todos os campos']);
-    exit;
-};
-
-/**
- * passowrd_hash - É uma função PHP que é usada para gerar um 'hash' seguro
- * 'hash' É uma versão CRIPTOGRAFADA e IRREVERSÍVEL da senha ORIGINAL
- * A função usa 'hashing fortes', como BCRYPT, para GARANTIR que as SENHAS sejam ARMAZENADAS DE MANEIRA SEGURA
- */
-$senhaHash = password_hash($senha, PASSWORD_BCRYPT);
-
-try {
-    // ABRINDO CONEXÃO
-    $sql = "INSERT INTO usuarios (email, senha, nome, cpf, celular, sexo, datanascimento, cep, endereco, complemento, bairro, cidade, estado, pjouclt ) VALUES (:email, :senha, :nome, :cpf, :celular, :sexo , :datanascimento, :cep, :endereco, :complemento, :bairro, :cidade, :estado, :pjouclt)";
-    $stmt = $pdo->prepare($sql);
+    // SE NÃO TIVER VALOR CAI NO NULL E É CONSIDERADO FALSO E CAI NO ERRO
+    if (!$email || !$senha || !$pjouclt || !$nome || !$cpf || !$celular || !$sexo || !$datanascimento || !$cep || !$endereco || !$complemento || !$bairro || !$cidade || !$estado) {
+        echo json_encode(['sucess' => false, 'message' => 'Preencha todos os campos']);
+        exit;
+    };
 
     /**
-     * bindParam nos PARÂMETROS 'email' e 'senha'
-     * Recebendo as variaveis $email e $senha
-     * Que estou enviando na API
+     * passowrd_hash - É uma função PHP que é usada para gerar um 'hash' seguro
+     * 'hash' É uma versão CRIPTOGRAFADA e IRREVERSÍVEL da senha ORIGINAL
+     * A função usa 'hashing fortes', como BCRYPT, para GARANTIR que as SENHAS sejam ARMAZENADAS DE MANEIRA SEGURA
      */
-    $stmt->bindParam(':nome', $nome);
-    $stmt->bindParam(':email', $email);
-    $stmt->bindParam(':senha', $senhaHash);
-    $stmt->bindParam(':cpf', $cpf);
-    $stmt->bindParam(':celular', $celular);
-    $stmt->bindParam(':sexo', $sexo);
-    $stmt->bindParam(':datanascimento', $datanascimento);
-    $stmt->bindParam(':cep', $cep);
-    $stmt->bindParam(':endereco', $endereco);
-    $stmt->bindParam(':complemento', $complemento);
-    $stmt->bindParam(':bairro', $bairro);
-    $stmt->bindParam(':cidade', $cidade);
-    $stmt->bindParam(':estado', $estado);
-    $stmt->bindParam(':pjouclt', $pjouclt);
+    $senhaHash = password_hash($senha, PASSWORD_BCRYPT);
 
-    if ($stmt->execute()) {
-        // Se der certo, executa esta mensagem de SUCESSO
-        echo json_encode(['sucess' => true, 'message' => 'Usuário cadastrado com sucesso!']);
-    } else {
-        //  Se der errado, retorna o ERRO
-        echo json_encode(['sucess' => false, 'message' => 'Erro ao cadastrar usuário']);
+    try {
+        // ABRINDO CONEXÃO
+        $sql = "INSERT INTO usuarios (email, senha, nome, cpf, celular, sexo, datanascimento, cep, endereco, complemento, bairro, cidade, estado, pjouclt ) VALUES (:email, :senha, :nome, :cpf, :celular, :sexo , :datanascimento, :cep, :endereco, :complemento, :bairro, :cidade, :estado, :pjouclt)";
+        $stmt = $pdo->prepare($sql);
+
+        /**
+         * bindParam nos PARÂMETROS 'email' e 'senha'
+         * Recebendo as variaveis $email e $senha
+         * Que estou enviando na API
+         */
+        $stmt->bindParam(':nome', $nome);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':senha', $senhaHash);
+        $stmt->bindParam(':cpf', $cpf);
+        $stmt->bindParam(':celular', $celular);
+        $stmt->bindParam(':sexo', $sexo);
+        $stmt->bindParam(':datanascimento', $datanascimento);
+        $stmt->bindParam(':cep', $cep);
+        $stmt->bindParam(':endereco', $endereco);
+        $stmt->bindParam(':complemento', $complemento);
+        $stmt->bindParam(':bairro', $bairro);
+        $stmt->bindParam(':cidade', $cidade);
+        $stmt->bindParam(':estado', $estado);
+        $stmt->bindParam(':pjouclt', $pjouclt);
+
+        if ($stmt->execute()) {
+            // Se der certo, executa esta mensagem de SUCESSO
+            echo json_encode(['sucess' => true, 'message' => 'Usuário cadastrado com sucesso!']);
+        } else {
+            //  Se der errado, retorna o ERRO
+            echo json_encode(['sucess' => false, 'message' => 'Erro ao cadastrar usuário']);
+        }
+    } catch (Exception $e) {
+        // SE OCORRER ALGUM ERRO DURANTO AS OUTRAS REQUISIÇÕES É RETORNADO ESTÁ
+        // CAPTURA ERROS GENÉRICOS
+        /**
+         * ERROS COMO:
+         * ERROS DE CONEXÃO
+         * ERROS AO ABRIR OU CRIAR ARQUIVO
+         * ERROS DE VALIDAÇÃO DE DADOS
+         * ERRO DE API
+         * ERRO DE TIPOS DE DADOS INESPERADOS
+         * ERRO DE CONFIGURAÇÕES OU AMBIENTE
+         * FALHA NO PROCESSAMENTO DE ARQUIVO OU DADOS
+         * ETC
+         */
+        echo json_encode(['sucess' => false, "message" => 'Erro ao cadastrar o usuário:' . $e->getMessage()]);
     }
-} catch (Exception $e) {
-    // SE OCORRER ALGUM ERRO DURANTO AS OUTRAS REQUISIÇÕES É RETORNADO ESTÁ
-    // CAPTURA ERROS GENÉRICOS
-    /**
-     * ERROS COMO:
-     * ERROS DE CONEXÃO
-     * ERROS AO ABRIR OU CRIAR ARQUIVO
-     * ERROS DE VALIDAÇÃO DE DADOS
-     * ERRO DE API
-     * ERRO DE TIPOS DE DADOS INESPERADOS
-     * ERRO DE CONFIGURAÇÕES OU AMBIENTE
-     * FALHA NO PROCESSAMENTO DE ARQUIVO OU DADOS
-     * ETC
-     */
-    echo json_encode(['sucess' => false, "message" => 'Erro ao cadastrar o usuário:' . $e->getMessage()]);
-}
+} elseif ($method == 'GET') {
+    #Lógica para Requisição GET
 
-?>
+    $email = $_GET['email'] ?? null;
+    if ($email) {
+        try {
+            $sql = "SELECT * FROM usuarios WHERE email = :email";
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindParam(':email', $email);
+
+            if ($stmt->execute()) {
+                $user = $stmt->fetch(PDO::FETCH_ASSOC);
+                if ($user) {
+                    echo json_encode(['sucess' => true, 'user' => $user]);
+                } else {
+                    echo json_encode(['sucess' => false, 'message' => 'Usuário não encontrado']);
+                }
+            } else {
+                echo json_encode(['sucess' => false, 'message' => 'Erro ao buscar usuário']);
+            }
+        } catch (Exception $e) {
+            echo json_encode(['sucess' => false, 'message' => 'Erro ao buscar o usuário:' . $e->getMessage()]);
+        }
+    } else {
+        echo json_encode(['sucess' => false, 'message' => 'Parâmetro "email" não fornecido']);
+    }
+}
